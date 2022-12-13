2e52952ae4e869f2608aee8a392b2296e8b0514c
Ueslem <ueslempagani@gmail.com>
2022-12-13 09:54:27 -0300

    complet

--- Exercicio 2 - CalculadoraAreaPerimetro.dart
+++ Exercicio 2 - CalculadoraAreaPerimetro.dart
@@ -1,32 +1,31 @@
 import 'dart:math' as math;
+
 class Circle {
 //  double? radius;
 //  double? pi;
- 
- double? calculatePerimeter({double? radius , double pi = 3.14}) {
-   if(radius != null && radius >= 0) {
-     return 2 * (pi * radius);
-   }
-   return null;
- }
- 
- double? calculateArea([double? radius,double pi = 3.14]) {
-   if(radius != null && radius >= 0) {
-     return pi * math.pow(radius, 2);
-   }
-   return null;
- }
- 
+
+  double? calculatePerimeter({double? radius, double pi = 3.14}) {
+    if (radius != null && radius >= 0) {
+      return 2 * (pi * radius);
+    }
+    return null;
+  }
+
+  double? calculateArea([double? radius, double pi = 3.14]) {
+    if (radius != null && radius >= 0) {
+      return pi * math.pow(radius, 2);
+    }
+    return null;
+  }
 }
 
 void main() {
- final calculateCircle = Circle();
- 
-  
- print(calculateCircle.calculatePerimeter(radius: 12 ));
- print(calculateCircle.calculateArea(10));
+  final calculateCircle = Circle();
+
+  print(calculateCircle.calculatePerimeter(radius: 12));
+  print(calculateCircle.calculateArea(10));
 //  calculateCircle.radius = 12;
 //  calculateCircle.pi = 3.14;
-  
+
 //  print(calculateCircle.perimetercalculate());
 }
